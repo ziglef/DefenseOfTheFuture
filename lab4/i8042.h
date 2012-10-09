570f6f0481ee4c731a7039d228ef394889356e7a
ei11010 <ei11010@5864e765-9ec0-484c-918b-0b1265aa1a1c>
2012-10-09 15:37:22 +0000

    tons of defines for KBD

--- lab4/i8042.h
+++ lab4/i8042.h
@@ -4,7 +4,23 @@ typedef struct{
 	int hook_id;
 } KeyBoardController;
 
-#define KBC_IRQ 	1			// The IRQ line going to be used by the KBC
-#define KBC_BIT 	1			// The hook_id for the KBC
+// KBC Constants
+#define KBC_IRQ 				1			// The IRQ line going to be used by the KBC
+#define KBC_BIT 				1			// The hook_id for the KBC
+#define KBC_IO_PORT				0x60		// Port for IN_BUF and OUT_BUF
 
+// C@KDB Outputs
+#define ACK						0xFA		// Acknowledgement by the C@KBD
+#define RESEND					0xFE		// C@KBD asking for a resend of the data
+#define ERROR					0xFC		// C@KBD reporting an error
+
+// Keyboard Commands
+#define KBD_RESET				0xFF		// Resets the KBD
+#define KBD_DEFAULT_ENABLE		0xF6		// Sets default values on the KBD and enables it
+#define KBD_DISABLE				0xF5		// Disables KBD (sets default values and stops scanning)
+#define KBD_CLEAR_ENABLE		0xF4		// Clears the buffer and enables KBD
+#define	KBD_RATE_DELAY			0xF3		// Changes KBD repetition rate/delay (bits 0-4 for rate and bits 5-6 for delay)
+#define KBD_LEDS				0xED		// Turns on/off the KBD LEDs uses bits 0-2
+
+// Time constant
 #define DELAY_US 	20000		// IBM's specification on the amount of time the i8042 waits for a command

25be73831782cc21a16a6bdeacc7f14c91f90b82
ei11010 <ei11010@5864e765-9ec0-484c-918b-0b1265aa1a1c>
2012-10-09 16:03:28 +0000

    KBC commands defines and such

--- lab4/i8042.h
+++ lab4/i8042.h
@@ -7,12 +7,35 @@ typedef struct{
 // KBC Constants
 #define KBC_IRQ 				1			// The IRQ line going to be used by the KBC
 #define KBC_BIT 				1			// The hook_id for the KBC
-#define KBC_IN_BUF				0x60		// Port for IN_BUF
+#define KBC_IO_BUF				0x60		// Port for IN_BUF
+#define KBC_STATUS				0x64		// Port for the STAT_REG and for issuing KBC commands (here arguments should be passed in the IN_BUF adress)
+/* KBC STAT_REG DATA:
+ * BIT 0: OBF If set, the OUT_BUF is full, thus data is available for reading.
+ * BIT 1: IBF If set, the IN_BUF is full, thus commands and arguments shouldn't be written.
+ * BIT 2: SYS 0 when system is in power-on reset, 1 when system already initialized.
+ * BIT 3: A2  Tells us if the A2 input line is a data byte(0) or a command byte(1).
+ * BIT 4: INH 0 if keyboard is inhibited, 1 otherwise.
+ * BIT 5: Aux Mouse data.
+ * BIT 6: Timeout If set means Timeout error, invalid data.
+ * BIT 7: Parity If set means Parity error - invalid data.
+ */
 
-// C@KDB Outputs
-#define ACK						0xFA		// Acknowledgement by the C@KBD
-#define RESEND					0xFE		// C@KBD asking for a resend of the data
-#define ERROR					0xFC		// C@KBD reporting an error
+// KBC Commands - Must be written to KBC_STATUS port, arguments if any are passed in KBC_IO_BUF by the IN_BUF, return values are passed in KBC_IO_BUF by the OUT_BUF
+#define	KBC_READ				0x20		// Reads command byte, returns command byte
+#define KBC_WRITE				0x60		// Writes command byte
+#define KBC_CHECK				0xAA		// Self-Test, returns KBC_OK if OK, KBC_ERROR if ERROR
+#define KBC_INT					0xAB		// Checks keyboard interface, returns 0 if OK
+#define KBC_DIS_INT				0xAD		// Disables KBD Interface
+#define KBC_EN_INT				0xAE		// Enables KBD Interface
+
+// KBC_CHECK returns
+#define KBC_OK					0x55		// The KBC is OK
+#define KBC_ERROR				0xFC		// The KBC is not OK
+
+// C@KBD Outputs
+#define CKBD_ACK				0xFA		// Acknowledgement by the C@KBD
+#define CKBD_RESEND				0xFE		// C@KBD asking for a resend of the data
+#define CKBD_ERROR				0xFC		// C@KBD reporting an error
 
 // Keyboard Commands
 #define KBD_RESET				0xFF		// Resets the KBD

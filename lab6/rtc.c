3c4a9b3a7f2a3312e6b1fa77dbc73c5a257b4924
ei11010 <ei11010@5864e765-9ec0-484c-918b-0b1265aa1a1c>
2012-11-07 18:08:52 +0000

    half

---
+++ lab6/rtc.c
@@ -0,0 +1,39 @@
+#include "rtc.h"
+
+void enable(){
+	endpoint_t ep;
+	char name[256];
+	int priv_f;
+
+	// FETCH OUR ENDPOINT
+	sys_whoami(&ep, name, 256, &priv_f);
+	// ENABLE IO-SENSITIVE OPERATIONS FOR OURSELVES
+	sys_enable_iop(ep);
+	asm("STI");
+}
+
+void disable(){
+	endpoint_t ep;
+	char name[256];
+	int priv_f;
+
+	// FETCH OUR ENDPOINT
+	sys_whoami(&ep, name, 256, &priv_f);
+	// ENABLE IO-SENSITIVE OPERATIONS FOR OURSELVES
+	sys_enable_iop(ep);
+	asm("CLI");
+}
+
+void rtc_read(unsigned long reg, unsigned long *byte){
+	disable();
+	sys_outb(RTC_ADDRESS_REG, reg);
+	sys_inb(RTC_DATA_REG, byte);
+	enable();
+}
+
+void rtc_write(unsigned long reg, unsigned long byte){
+	disable();
+	sys_outb(RTC_ADDRESS_REG, rtc_reg);
+	sys_outb(RTC_DATA_REG, byte);
+	enable();
+}
--- lab6/rtc.h
+++ lab6/rtc.h
@@ -1,2 +1,47 @@
-#define RTC_ADDR_REG			0x70
+// RTC DEFINES AND CONSTANTS
+
+#define BIT(n) 					(0x01<<(n))
+
+// RTC PORTS
+#define RTC_ADDRESS_REG			0x70
 #define RTC_DATA_REG			0x71
+
+// RTC REGISTER A
+#define RTC_UIP					BIT(7)
+#define RTC_DV2					BIT(6)
+#define RTC_DV1					BIT(5)
+#define RTC_DV0					BIT(4)
+#define RTC_RS3					BIT(3)
+#define RTC_RS2					BIT(2)
+#define RTC_RS1					BIT(1)
+#define RTC_RS0					BIT(0)
+
+// RTC REGISTER B
+#define RTC_SET					BIT(7)
+#define RTC_PIE					BIT(6)
+#define RTC_AIE					BIT(5)
+#define RTC_UIE					BIT(4)
+#define RTC_SQWE				BIT(3)
+#define RTC_DM					BIT(2)
+#define RTC_2412				BIT(1)
+#define RTC_DSE					BIT(0)
+
+// RTC REGISTER C
+#define RTC_IRQF				BIT(7)
+#define RTC_PF					BIT(6)
+#define RTC_AF					BIT(5)
+#define RTC_UF					BIT(4)
+#define RTC_C_3					BIT(3)
+#define RTC_C_2					BIT(2)
+#define RTC_C_1					BIT(1)
+#define RTC_C_0					BIT(0)
+
+// RTC REGISTER D
+#define RTC_VRT					BIT(7)
+#define RTC_D_6					BIT(6)
+#define RTC_D_5					BIT(5)
+#define RTC_D_4					BIT(4)
+#define RTC_D_3					BIT(3)
+#define RTC_D_2					BIT(2)
+#define RTC_D_1					BIT(1)
+#define RTC_D_0					BIT(0)

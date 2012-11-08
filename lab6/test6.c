9f1265c6db9c9bb55afc4f85afa08f2942e3ee14
ei11167 <ei11167@5864e765-9ec0-484c-918b-0b1265aa1a1c>
2012-11-08 11:20:23 +0000

    test6.c updated

--- lab6/test6.c
+++ lab6/test6.c
@@ -8,7 +8,12 @@ int test_conf(void) {
 	rtc_read(RTC_REGC,&regC);
 	rtc_read(RTC_REGD,&regD);
 
-	printf("REGISTER A: 0x%X\nREGISTER B: 0x%X\nREGISTER C: 0x%X\nREGISTER D: 0x%X", regA, regB, regC, regD);
+	printf("REGISTER A: 0x%X\nREGISTER B: 0x%X\nREGISTER C: 0x%X\nREGISTER D: 0x%X\n", regA, regB, regC, regD);
+	printf("REG A FLAGS: UIP: %d  DV2: %d  DV1: %d  DV0: %d  RS3: %d  RS2: %d  RS1: %d  RS0: %d\n", ((regA&RTC_UIP)&&RTC_UIP), ((regA & RTC_DV2)&&RTC_DV2), ((regA & RTC_DV1)&&RTC_DV1), ((regA & RTC_DV0)&&RTC_DV0), ((regA & RTC_RS3)&&RTC_RS3), ((regA & RTC_RS2)&&RTC_RS2), ((regA & RTC_RS1)&&RTC_RS1), ((regA & RTC_RS0)&&RTC_RS0));
+	printf("REG B FLAGS: SET: %d  PIE: %d  AIE: %d  UIE: %d  SQWE: %d  DM: %d  24/12: %d  DSE: %d\n", ((regB&RTC_SET)&&RTC_SET), ((regB & RTC_PIE)&&RTC_PIE), ((regB & RTC_AIE)&&RTC_AIE), ((regB & RTC_UIE)&&RTC_UIE), ((regB & RTC_SQWE)&&RTC_SQWE), ((regB & RTC_DM)&&RTC_DM), ((regB & RTC_2412)&&RTC_2412), ((regB & RTC_DSE)&&RTC_DSE));
+	printf("REG C FLAGS: IRQF: %d  PF: %d  AF: %d  UF: %d\n",((regC&RTC_IRQF)&&RTC_IRQF), ((regC & RTC_PF)&&RTC_PF), ((regC & RTC_AF)&&RTC_AF), ((regC & RTC_UF)&&RTC_UF));
+	printf("REG D FLAGS: VRT: %d\n", ((regD&RTC_VRT)&&RTC_VRT));
+
 	return 0;
 }
 
@@ -32,7 +37,7 @@ int test_date(void) {
 				switch(_ENDPOINT_P(msg.m_source)){
 					case HARDWARE:
 						if(msg.NOTIFY_ARG & RTC_BIT_MASK){
-							rtc_handler();
+							if(rtc_handler()) break;
 						}
 						break;
 					default:

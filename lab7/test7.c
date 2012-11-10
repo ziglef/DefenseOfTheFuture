0eed7bd6ce2fbd466cd1c5182a8ab94c2605e2ca
ei11010 <ei11010@5864e765-9ec0-484c-918b-0b1265aa1a1c>
2012-11-10 17:13:52 +0000

    start

---
+++ lab7/test7.c
@@ -0,0 +1,20 @@
+int test_conf(unsigned short base_addr) {
+    /* To be completed */
+}
+
+int test_set(unsigned short base_addr, unsigned long bits, unsigned long stop, 
+	     long parity, unsigned long rate) { 
+    /* To be completed */
+}
+
+int test_poll(/* details to be provided */) { 
+    /* To be completed */
+}
+
+int test_int(/* details to be provided */) { 
+    /* To be completed */
+}
+
+int test_fifo(/* details to be provided */) {
+    /* To be completed */
+}

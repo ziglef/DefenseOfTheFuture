1f8ffcc5e73171074e79aaa3e290c755db2fd0e6
ei11010 <ei11010@5864e765-9ec0-484c-918b-0b1265aa1a1c>
2012-11-14 17:32:16 +0000

    test7.c -fixed

--- lab7/test7.c
+++ lab7/test7.c
@@ -29,8 +29,57 @@ int test_conf(unsigned short base_addr) {
     printf("LCR: 0x%X\n", LCR);
     printf("MCR: 0x%X\n", MCR);
     printf("SR: 0x%X\n", SR);
-    printf("DLL: 0x%X\n", DLL);
+    printf("DLL: 0x%X\n", DLL); // passar para baixo
     printf("DLH: 0x%X\n", DLH);
+
+    int wordlength;
+    if((LCR&0x03)==0x00)
+    	wordlength = 5;
+    else if((LCR&0x03)==0x01)
+    	wordlength = 6;
+    else if((LCR&0x03)==0x02)
+    	wordlength = 7;
+    else if((LCR&0x03)==0x03)
+    	wordlength = 8;
+    printf("Word Length Select: %d\n", wordlength);
+
+    int n_stop_b;
+    if((LCR&0x04)== 0x00)
+    	n_stop_b = 0;
+    else if((LCR&0x04)== 0x01)
+    	n_stop_b = 1;
+    printf("No. of stop bits: %d\n", n_stop_b);
+
+   if(((LCR&0x38) == 0x01))
+	   printf("Parity: Odd\n");
+   else if((LCR&0x38) == 0x03)
+	   printf("Parity: Even\n");
+   else if((LCR&0x38) == 0x05)
+	   printf("Parity: 1\n");
+   else if((LCR&0x38) == 0x07)
+	   printf("Parity: 0\n");
+   else
+	   printf("Parity: None\n");
+
+   unsigned int bitrate, DL;
+
+   DL = DLH;
+   DL = DL << 8;
+   DL = DL + DLL;
+   bitrate = 115200 / DL;
+
+   printf("Bitrate: %d\n", bitrate);
+
+   if((IER&0x01) == 0x01)
+	   printf("Rx Interrupts: Enabled\n");
+   else
+	   printf("Rx Interrupts: Disabled\n");
+
+   if((IER&0x02) == 0x02)
+   	   printf("Tx Interrupts: Enabled\n");
+   else
+	   printf("Tx Interrupts: Disabled\n");
+
 }
 
 int test_set(unsigned short base_addr, unsigned long bits, unsigned long stop, long parity, unsigned long rate) {
